622a9509a5a17696872014400315cc325a738105
Matthew Mazankowski <m.g.mazankowski@gmail.com>
2022-01-26 22:58:43 -0700

    large comit

---
+++ Mission4Assignment/Models/MovieFormContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mission4Assignment.Models
+{
+    public class MovieFormContext : DbContext
+    {
+        //Constructor
+        public MovieFormContext(DbContextOptions<MovieFormContext> options) : base(options)
+        {
+            //leave blank for now
+        }
+
+        public DbSet<FormResponses> Responses { get; set; }
+
+    }
+}
+
